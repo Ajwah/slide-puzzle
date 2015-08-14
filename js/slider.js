d805197137f588ba378f32542de0aa8fc7cc98f3
Ajwah <johnson786.kevin+git@gmail.com>
2015-08-13 21:04:02 -0400

    Implement controller.

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 /**
- * IIFE representing the model that will make certain functions public under namespace Model
+ * IIFE representing the model that will make certain functions public under namespace M
  * @return N/A
  */
 (function(){
@@ -19,8 +19,8 @@
    * @return {Record of int} w: amount blocks width-wise, h: # blocks height wise, t: total
    */
   var getRatio = function(){
-    var h = +$('img').attr("ratio").split(':')[0]
-        w = +$('img').attr("ratio").split(':')[1];
+    var h = +$('img').attr("ratio").split(':')[1]
+        w = +$('img').attr("ratio").split(':')[0];
     return {w: w, h: h, t: h*w};
   };
 
@@ -32,13 +32,52 @@
     return $('img').attr('src');
   };
 
-  window.Model = {
+  window.M = {
     getDim: getDim,
     getRatio: getRatio,
     getUrl: getUrl
   };
 })();
 
+/**
+ * IIFE representing Controller. Functions publicaly available through namespace C
+ * @return N/A
+ */
+(function(){
+
+  /**
+   * Obtain url of img file
+   * @return {String} path to img file
+   */
+  var getUrl = function(){
+    return M.getUrl()
+  };
+
+  /**
+   * Obtain the step-size/amount that need to be taken to cover img area
+   * according to the ratio in both dimensions.
+   * @return {Record of int} w: step-size width-wise
+   *                         h: step-size height-wise
+   *                         aw: amount of steps width-wise
+   *                         ah: amount of steps height-wise
+   */
+  var getSteps = function(){
+    var d = M.getDim(),
+        r = M.getRatio();
+    return {w: Math.floor(d.w/r.w),
+            h: Math.floor(d.h/r.h),
+            aw: r.w,
+            ah: r.h
+           };
+  };
+
+  window.C = {
+    getUrl: getUrl,
+    getSteps: getSteps
+  };
+})();
+
+
 $(document).ready(
-  console.log(Model.getUrl())
+  console.log(C.getSteps())
 );
