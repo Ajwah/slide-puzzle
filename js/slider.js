00496594a84a786fd99a6923762da3e78130b251
Ajwah <johnson786.kevin+git@gmail.com>
2015-08-17 17:28:46 -0400

    Refactor the code to reflect MVC paradigm more closely and removal of duplicate code.

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,26 @@
+var img = $('img');
 $(document).ready(
-$('img').load(function(){ // Wait until image is fully loaded.
+img.load(function(img){ // Wait until image is fully loaded.
 /**
  * IIFE representing the model that will make certain functions public under namespace M
  * @return N/A
  */
 (function(){
+  var dim = {},
+      ratio = {};
   /**
    * Obtain the size dimension attributes of img
    * @return {Record of int} w: size width-wise, h: size height-wise
    */
   var getDim = function(){
-    var h = document.querySelector('img').naturalHeight, //get actual height regardless of css selector
-        w = document.querySelector('img').naturalWidth;
-    return {w: w, h: h};
+    var h, w;
+    if (Object.keys(dim).length === 0) {
+      h = img.prop('naturalHeight'), //get actual height regardless of css selector
+      w = img.prop('naturalWidth');
+      dim.w = w;
+      dim.h = h;
+    }
+    return dim;
   };
 
   /**
@@ -21,9 +29,14 @@ $('img').load(function(){ // Wait until image is fully loaded.
    * @return {Record of int} w: amount blocks width-wise, h: # blocks height wise
    */
   var getRatio = function(){
-    var h = +$('img').attr("ratio").split(':')[1],
-        w = +$('img').attr("ratio").split(':')[0];
-    return {w: w, h: h};
+    var h,w;
+    if (Object.keys(ratio).length === 0) {
+      h = +img.attr("ratio").split(':')[1],
+      w = +img.attr("ratio").split(':')[0];
+      ratio.h = h;
+      ratio.w = w;
+    }
+    return ratio
   };
 
   /**
@@ -31,7 +44,7 @@ $('img').load(function(){ // Wait until image is fully loaded.
    * @return {String} path of image file
    */
   var getUrl = function(){
-    return $('img').attr('src');
+    return img.attr('src');
   };
 
   window.M = {
@@ -40,12 +53,36 @@ $('img').load(function(){ // Wait until image is fully loaded.
     getUrl: getUrl
   };
 })();
+/*
+(function(){
+  var positions = [],
+      slides = [];
 
+  var savePos = function(w,h){
+    positions.push({top:h, left: w});
+  }
+
+  window.M_slides = {
+    savePos: savePos
+  };
+})();
+*/
 /**
- * IIFE representing Controller. Functions publicaly available through namespace C
+ * IIFE representing Controller. Functions publicly available through namespace C
  * @return N/A
  */
 (function(){
+  var positions = [],
+      container = $("#puzzle"),
+      imgContainer = container.find("figure");
+
+  /**
+   * Return html image container
+   * @return {JQuery object}
+   */
+  var getImgContainer = function(){
+    return imgContainer;
+  };
 
   /**
    * Obtain url of img file
@@ -75,65 +112,14 @@ $('img').load(function(){ // Wait until image is fully loaded.
            };
   };
 
-  window.C = {
-    getUrl: getUrl,
-    getSteps: getSteps
-  };
-})();
-
-/**
- * IIFE representing View
- * @return N/A
- */
-(function(){
-  var url, s,
-      idCounter = 0,
-      piece = $("<div>"),
-      container = $("#puzzle"),
-      imgContainer = container.find("figure"),
-      img = imgContainer.find("img"),
-      positions = [];
-
   /**
-   * Place a rectangular selection portion of original image at designated coordinates w,h
+   * Save position of slide according to w and h
    * @param  {int} w width
    * @param  {int} h height
    * @return N/A
-   * @effect Manipulation of DOM
    */
-  var _putImg = function(w,h){
-    piece.clone()
-         .attr("id", idCounter++)
-         .css({
-                width: s.w,
-                height: s.h,
-                position: "absolute",
-                top: h,
-                left: w,
-                backgroundImage: ["url(", url, ")"].join(""),
-                backgroundPosition: [
-                  "-", w, "px ",
-                  "-", h, "px"].join("")
-              }).appendTo(imgContainer);
-  };
-
-  /**
-   * Slice up original image into sub selections that can be slided.
-   * @return N/A
-   * @effect Calls on _putImg
-   *         positions updated
-   */
-  var _sliceImg = function(){
-    var w,h,
-        totalAmountPieces = s.t,
-        w_end = s.w*s.aw,
-        h_end = s.h*s.ah;
-    for (h=0;h<h_end;h+=s.h) {
-      for (w=0;w<w_end;w+=s.w) {
-        _putImg(w,h);
-        positions.push({top: h, left: w});
-      }
-    }
+  var savePos = function(w,h){
+    positions.push({top:h, left: w});
   };
 
   /**
@@ -165,15 +151,36 @@ $('img').load(function(){ // Wait until image is fully loaded.
 
     slides = _shuffle(slides);
 
-    //Assign the ordered positions to the shuffled slides.
-    $.each(slides, function (i) {
-        slides.eq(i).css(positions[i]);
-    });
-    slides.appendTo(imgContainer);
+    //Assign the original positions to the shuffled slides.
+    V.displaySlides(slides, positions);
   };
 
+  /**
+   * Initialize the whole game which requires:
+   *   1. Slice up the image according to ratio
+   *   2. Remove initial slide to enable sliding
+   *   3. Scramble the slides when start is pressed
+   *   4. Start the timer
+   *   5. Allow to start playing
+   * @return N/A
+   */
+  var init = function(){
+    V.init();
+    V.removeInitialSlide(container);
+    positions.shift();
+    $("#start").on("click", _scrambleSlides);
+    _play();
+  };
+
+  /**
+   * Constrain the various dragging movements that can be made with the various slides
+   * @return N/A
+   * @effect Manipulation DOM
+   */
   var _play = function(){
-    var slides = imgContainer.children();
+    var slides = imgContainer.children(),
+        s = getSteps();
+
     slides.draggable({
         containment: "parent",
         grid: [s.w,s.h],
@@ -189,27 +196,112 @@ $('img').load(function(){ // Wait until image is fully loaded.
       });
   };
 
+  window.C = {
+    init: init,
+    getUrl: getUrl,
+    getSteps: getSteps,
+    savePos: savePos,
+    getImgContainer: getImgContainer
+  };
+})();
+
+/**
+ * IIFE representing View
+ * @return N/A
+ */
+(function(){
+  var url, s,
+      piece = $("<div>"),
+      imgContainer = C.getImgContainer();
+  /**
+   * Place a rectangular selection portion of original image at designated coordinates w,h
+   * @param  {int} w width
+   * @param  {int} h height
+   * @return N/A
+   * @effect Manipulation of DOM
+   */
+  var _putImg = function(id,w,h){
+    piece.clone()
+         .attr("id", id)
+         .css({
+                width: s.w,
+                height: s.h,
+                position: "absolute",
+                top: h,
+                left: w,
+                backgroundImage: ["url(", url, ")"].join(""),
+                backgroundPosition: [
+                  "-", w, "px ",
+                  "-", h, "px"].join("")
+              }).appendTo(imgContainer);
+  };
+
+  /**
+   * Slice up original image into sub selections that can be slided.
+   * @return N/A
+   * @effect Calls on _putImg
+   *         positions updated
+   */
+  var _sliceImg = function(){
+    var w,h,
+        id = 0,
+        totalAmountPieces = s.t,
+        w_end = s.w*s.aw,
+        h_end = s.h*s.ah;
+
+    for (h=0;h<h_end;h+=s.h) {
+      for (w=0;w<w_end;w+=s.w) {
+        _putImg(id,w,h);
+        C.savePos(w,h);
+        id++;
+      }
+    }
+  };
+
+  /**
+   * Display slides according to positions
+   * @param  {Array} slides    Various slides from DOM tree that previously have been shuffled.
+   * @param  {Array} positions Initial positions of the slides at start of game from top left to bottom right.
+   * @return N/A
+   * @effect Manipulate DOM
+   */
+  var displaySlides = function(slides, positions){
+    $.each(slides, function (i) {
+      slides.eq(i).css(positions[i]);
+    });
+    // slides.appendTo(imgContainer);
+  };
+
+  /**
+   * Remove an initial slide from the img so as to make sliding plausible.
+   * @param  {Array} container ?
+   * @return N/A
+   * @effect Removal of initial slide from DOM.
+   */
+  var removeInitialSlide = function(container){
+    container.find("#0").remove();
+  };
+
   /**
    * Initialize the view
    * @return N/A
+   * @effect Removal original img
+   *         Manipulation DOM
    */
   var init = function(){
     url = C.getUrl();
     s = C.getSteps();
     _sliceImg();
     img.remove();
-    container.find("#0").remove();
-    positions.shift();
-
-    $("#start").on("click", _scrambleSlides);
-    _play();
   };
 
   window.V = {
-    init: init
+    init: init,
+    displaySlides: displaySlides,
+    removeInitialSlide: removeInitialSlide
   };
 })();
 
-V.init();
+C.init();
 
-}));
+}(img)));
