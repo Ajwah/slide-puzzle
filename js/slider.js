0c924e0038745493493cfee6ca8852a668a3d68b
Ajwah <johnson786.kevin+git@gmail.com>
2015-08-20 04:23:48 -0400

    Further refactor code and implement fully working draggable. Include .jshintrc and fix use strict violations.

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,30 @@
-var img = $('img'); //Global variable intended to be passed through as parameter of anonymous function below.
+"use strict";
+var img = $("img"); //Global variable intended to be passed through as parameter of anonymous function below.
 
 //Only start processing once the entire DOM, image etc. has loaded
 //which for this implementation is necessary to avoid trying to read
-//properties of image that hasn't been loaded as yet.
+//properties of image that haven"t been loaded as yet.
 window.onload = function(img){
 /**
  * IIFE representing the model that will make certain functions public under namespace M
  * @return N/A
  */
 (function(){
-  var dim = {},
-      ratio = {};
+  var _dim = {},   //Storage to remember dim and ratio of img even after
+      _ratio = {}; //the img has been deleted from DOM.
   /**
    * Obtain the size dimension attributes of img
    * @return {Record of int} w: size width-wise, h: size height-wise
    */
   var getDim = function(){
     var h, w;
-    if (Object.keys(dim).length === 0) {
-      h = img.prop('naturalHeight'), //get actual height regardless of css selector
-      w = img.prop('naturalWidth');
-      dim.w = w;
-      dim.h = h;
+    if (Object.keys(_dim).length === 0) {
+      h = img.prop("naturalHeight"); //get actual height regardless of css selector
+      w = img.prop("naturalWidth");
+      _dim.w = w;
+      _dim.h = h;
     }
-    return dim;
+    return _dim;
   };
 
   /**
@@ -33,13 +34,13 @@ window.onload = function(img){
    */
   var getRatio = function(){
     var h,w;
-    if (Object.keys(ratio).length === 0) {
-      h = +img.attr("ratio").split(':')[1],
-      w = +img.attr("ratio").split(':')[0];
-      ratio.h = h;
-      ratio.w = w;
+    if (Object.keys(_ratio).length === 0) {
+      h = +img.attr("ratio").split(":")[1];
+      w = +img.attr("ratio").split(":")[0];
+      _ratio.h = h;
+      _ratio.w = w;
     }
-    return ratio
+    return _ratio;
   };
 
   /**
@@ -47,7 +48,7 @@ window.onload = function(img){
    * @return {String} path of image file
    */
   var getUrl = function(){
-    return img.attr('src');
+    return img.attr("src");
   };
 
   window.M = {
@@ -56,26 +57,14 @@ window.onload = function(img){
     getUrl: getUrl
   };
 })();
-/*
-(function(){
-  var positions = [],
-      slides = [];
 
-  var savePos = function(w,h){
-    positions.push({top:h, left: w});
-  }
-
-  window.M_slides = {
-    savePos: savePos
-  };
-})();
-*/
 /**
  * IIFE representing Controller. Functions publicly available through namespace C
  * @return N/A
  */
 (function(){
   var positions = [],
+      emptySquare,
       container = $("#puzzle"),
       imgContainer = container.find("figure");
 
@@ -92,7 +81,7 @@ window.onload = function(img){
    * @return {String} path to img file
    */
   var getUrl = function(){
-    return M.getUrl()
+    return M.getUrl();
   };
 
   /**
@@ -113,6 +102,17 @@ window.onload = function(img){
             ah: r.h,
             t: r.w * r.h
            };
+  };
+
+  /**
+   * Remove initial square from the board and return the css coordinates thereof.
+   * @return {Object} coordinates according to css {top:_,left:_}
+   */
+  var _popInitialSquare = function(){
+    var _initialSquare;
+    V.removeInitialSlide(container);
+    _initialSquare = positions.shift();
+    return _initialSquare;
   };
 
   /**
@@ -159,6 +159,179 @@ window.onload = function(img){
   };
 
   /**
+   * Obtain the slides directly adjacent to the empty cavity on the board
+   * according to the 4 primary directions (N,E,S,W).
+   * According to the position of the empty cavity on the board, these adjacent
+   * slides can amount to 2, 3 or 4 in number.
+   * @return {Array of JQuery objects}        The adjacent slides relative to the empty square.
+   */
+  var _getAdjacentSlides = function(){
+    //Obtain all the slides currently on the board.
+    var slides = imgContainer.children(),
+        s = getSteps();
+    /**
+     * Helper function for the mapping below that given the coordinates of
+     * the empty square on the board will calculate and return an object with
+     * the coordinates that logically apply to an adjacent neighbour -i- thereof.
+     *
+     * Following diagram depicts all the possible neighours -i- any empty square on the
+     * board theoretically speaking may have:
+     *                                        3
+     *                                      2 # 0
+     *                                        1
+     *                                      where # represents the empty square
+     *                                      and 0,1,2,3 represent clockwise the
+     *                                      potential neighbours thereof.
+     * Neighbour 0,1 require 1 positive square distance away from #, in dimensions x and y respectively.
+     * Neighbour 2,3 require 1 negative square distance away from #, in dimensions x and y respectively.
+     *
+     * @param  {Object} e Representing the empty square
+     * @param  {int}    i Representing a neighbour from 0 to 3
+     * @return {Object}   Encompassing the coordinates such a neighbour -i- should
+     *                    have relative to the empty square. This includes coordinates
+     *                    that are outside the board which will need to be discriminated
+     *                    against seperately.
+     */
+    var getAdjacentSquare = function(e,i){
+      var sign = (i<2)? 1 : -1, //0,1 is always a positive square distance
+                                //whereas 2,3 are negative square distance
+          //only 0 and 2 maintain that square distance horizontally
+          left = (i%2===0)? e.left + (sign*s.w) : e.left,
+          //only 1 and 3 maintain that square distance vertically
+          top = (i%2!==0)? e.top + (sign*s.h) : e.top;
+      return {left:left,top:top};
+    };
+
+    /**
+     * Helper function to assist in the filtering below that given two objects e1 and e2,
+     * both that represent the coordinates of a slide, will return if they equal or not.
+     * @param  {Object} e1 coordinates according to css {top:_, left:_}
+     * @param  {Object} e2 coordinates according to css {top:_, left:_}
+     * @return {Boolean}   Confirm if e1,e2 are same or different.
+     */
+    var compare = function(e1,e2){
+      return (e1.left === e2.left && e1.top === e2.top);
+    };
+
+    // Create an array of 4 elements, prefilled with emptySquare.
+    // e.g. say emptySquare is {top:0,left:0} then the array is
+    // [{top:0,left:0},{top:0,left:0},...]
+    var emptySquares = Array.apply(null, new Array(4))
+                            .map(Object.prototype.valueOf,emptySquare),
+    //map the above initialized array to a new array -adjacent- containing all the
+    //logic neighbours relative to the empty square, including out of bound ones.
+        adjacent = emptySquares.map(getAdjacentSquare),
+    //Filter out from those logic neighbours only those that are present on board.
+        adjacentSlides = slides.filter(function(i){
+          //For every slide, check if its css position equals to any of the logic neighbours.
+          return adjacent.reduce(function(acc,v){
+            return acc || compare(slides.eq(i).position(),v);
+          }, false);
+        });
+    return adjacentSlides;
+  };
+
+  /**
+   * Set up the draggable jquery-ui handler and limit the usage thereof:
+   *           -1 to the direct adjacent neighbours of the empty square
+   *           -2 to slide into the empty square leaving behind a new empty square
+   * Keep on playing until the original picture is obtained.
+   * @param  {JQuery object} adjacentSlides encompasses all the actual adjacent neighbours to empty square
+   * @return N/A
+   * @effect Manipulation of DOM
+   */
+  var _play = function(adjacentSlides){
+    //Obtain all the slides present on the board.
+    var slides = imgContainer.children();
+    /**
+     * Helper function to determine if a particular slide has actually moved from its original place.
+     * @param  {Object}  originalPos coordinates according to css notation {top:_,left:_}
+     * @param  {Object}  ui_pos      coordinates according to css notation {top:_,left:_}
+     * @return {Boolean}             Evaluate if originalPos equals to ui_pos.
+     */
+    var _isSlided = function(originalPos, ui_pos){
+      return (originalPos.left !== ui_pos.left) ||
+             (originalPos.top !== ui_pos.top);
+    };
+    //For debugging purposes to be removed soon.
+    slides.css("border", "0px solid red");
+
+    //Set up draggable event handler for each of the adjacent slides.
+    $.each(adjacentSlides, function (i) {
+      var that = adjacentSlides.eq(i),
+          pos = that.position(),
+          //Determine offset of css position of slides relative to the window which is the relative
+          //position of the image container to the window.
+          offset = {left: that.parent().offset().left,
+                    top: that.parent().offset().top},
+          x = [emptySquare.left+offset.left+1,pos.left+offset.left+1],
+          y = [emptySquare.top+offset.top+1,pos.top+offset.top+1],
+          //Containment is a draggable parameter that can be submitted to constrain the movement of the
+          //draggable to a specific area. In this case the usage can be depicted as follows:
+          //           3
+          //         2 # 0
+          //           1        ___
+          //           For 0 : |# 0|
+          //                    ---
+          //                    ___
+          //           For 2 : |2 #|
+          //                    ---
+          //                    _
+          //           For 1 : |#|
+          //                   |1|
+          //                    -
+          //
+          //                    -
+          //           For 3 : |3|
+          //                   |#|
+          //                    -
+          //For each neighbour 0,1,2,3 a separate containment is defined that reflects the depiction above
+          //so that the domain of the draggable is limited to the boxed area.
+          //
+          //Important Bug Note: Containment demands left to right, top to bottom orientation of the
+          //                    coordinates, from smaller to bigger or else the draggable will not work
+          //                    correctly. This orientation is assured below as follows:
+          containment = [Math.min.apply(null, x), Math.min.apply(null, y),
+                         Math.max.apply(null, x), Math.max.apply(null, y)];
+      //For debugging purposes, to be removed soon.
+      that.css("border", "5px solid red");
+
+      //For each neighbour define a draggable with its personalized containment.
+      that.draggable({
+        distance: 5,
+        disabled: false,
+        //Parameter below not part of jquery-ui draggable API specification and added for convenience.
+        originalPos: {left: pos.left, top: pos.top},
+        //define containment area.
+        containment: containment,
+        stop: function(e, ui){
+          var originalPos = that.draggable("option", "originalPos");
+          //At the slightest registration of a movement, complete the movement
+          //so as to fill the empty square.
+          if (_isSlided(originalPos, ui.position)) {
+            //Update original position of the slide.
+            that.draggable("option", "originalPos", {left: ui.position.left, top: ui.position.top});
+            //Put slide on place of empty square.
+            V.putSlide(that, {left: emptySquare.left, top: emptySquare.top});
+            //that.css({left: emptySquare.left, top: emptySquare.top});
+            //Update position of empty square.
+            emptySquare.left = originalPos.left;
+            emptySquare.top = originalPos.top;
+
+            //Disable the draggable for every adjacent slide.
+            $.each(adjacentSlides, function (i) {
+              adjacentSlides.eq(i).draggable("disable");
+            });
+
+            //Repeat with updated board.
+            _play(_getAdjacentSlides());
+          }
+        }
+      });
+    });
+  };
+
+  /**
    * Initialize the whole game which requires:
    *   1. Slice up the image according to ratio
    *   2. Remove initial slide to enable sliding
@@ -168,35 +341,14 @@ window.onload = function(img){
    * @return N/A
    */
   var init = function(){
+    var _initialSquare;
     V.init();
-    V.removeInitialSlide(container);
-    positions.shift();
-    $("#start").on("click", _scrambleSlides);
-    _play();
-  };
-
-  /**
-   * Constrain the various dragging movements that can be made with the various slides
-   * @return N/A
-   * @effect Manipulation DOM
-   */
-  var _play = function(){
-    var slides = imgContainer.children(),
-        s = getSteps();
-
-    slides.draggable({
-        containment: "parent",
-        grid: [s.w,s.h],
-        start: function(e, ui){
-          console.log('Start to drag');
-        },
-        drag: function(e, ui) {
-          console.log('Dragging...');
-        },
-        stop: function(e, ui){
-          console.log('Stop dragging.');
-        }
-      });
+    _initialSquare = _popInitialSquare();
+    $("#start").on("click", function(){
+      emptySquare = _initialSquare;
+      _scrambleSlides();
+      _play(_getAdjacentSlides());
+    });
   };
 
   window.C = {
@@ -213,7 +365,8 @@ window.onload = function(img){
  * @return N/A
  */
 (function(){
-  var url, s,
+  var url = C.getUrl(),
+      s = C.getSteps(),
       piece = $("<div>"),
       imgContainer = C.getImgContainer();
   /**
@@ -259,6 +412,7 @@ window.onload = function(img){
         id++;
       }
     }
+    console.log(id === totalAmountPieces); //Better served with an assert.
   };
 
   /**
@@ -270,14 +424,24 @@ window.onload = function(img){
    */
   var displaySlides = function(slides, positions){
     $.each(slides, function (i) {
-      slides.eq(i).css(positions[i]);
+      putSlide(slides.eq(i),positions[i]);
     });
-    // slides.appendTo(imgContainer);
+  };
+
+  /**
+   * Put an individual slide to the designated coordinates.
+   * @param  {Jquery Object} slide    Slide to be depicted
+   * @param  {Object} position Conforming css notation
+   * @return N/A
+   * @effect Manipulation DOM
+   */
+  var putSlide = function(slide, position){
+    slide.css(position);
   };
 
   /**
    * Remove an initial slide from the img so as to make sliding plausible.
-   * @param  {Array} container ?
+   * @param  {JQuery Object} container [In charge of the DOM portion that encapsulates the img and timer]
    * @return N/A
    * @effect Removal of initial slide from DOM.
    */
@@ -292,8 +456,6 @@ window.onload = function(img){
    *         Manipulation DOM
    */
   var init = function(){
-    url = C.getUrl();
-    s = C.getSteps();
     _sliceImg();
     img.remove();
   };
@@ -301,6 +463,7 @@ window.onload = function(img){
   window.V = {
     init: init,
     displaySlides: displaySlides,
+    putSlide: putSlide,
     removeInitialSlide: removeInitialSlide
   };
 })();
